fbab824cb35198606b192d93d7f0bceee6967129
Ryan Griffith <rc.griff@gmail.com>
2011-01-02 21:02:09 -0500

    Initial commit of the plugin.
    Signed-off-by: Ryan Griffith <rc.griff@gmail.com>

---
+++ fabricated-events.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Fabricated Events
+Plugin URI: #
+Description: An easy to use event management plugin that utilizes new features added in Wordpress v3.0.
+Author: Ryan Griffith
+Version: 1.0
+Author URI: http://www.griffworks.com/
+*/
+
+define('FEM_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+require_once('inc/core.php');
+$events = new FabricatedEvents();
+
+add_action('init', array(&$events, 'createEventPostType'));
+add_action('admin_init', array(&$events, 'createEventMetaboxes'));
+
+
+
+
+
+?>
---
+++ js/core.js
@@ -0,0 +1,3 @@
+jQuery(function() {
+	jQuery( ".datepicker" ).datepicker();
+});
