7553b169b672a20ed95a3f481e1e98b0b922bc29
Ryan Griffith <rc.griff@gmail.com>
2011-01-16 16:59:08 -0500

    Updated the processing for the custom post meta.
    Signed-off-by: Ryan Griffith <rc.griff@gmail.com>

--- inc/core.php
+++ inc/core.php
@@ -113,96 +113,6 @@ if (!class_exists('FabricatedEvents')) {
 		
 			// Use nonce for verification
 		  	wp_nonce_field( FABRICATEDEVENTS_PLUGIN_URL, 'fabricatedevents_noncename' );
-<<<<<<< HEAD
-			
-			?><h4>Time &amp; Date</h4>
-			<table class="form-table"> 
-				<tr>
-					<th scope="row"><label for="start-date"><?php echo _e('Start Date');?></label></th>
-					<td><?php echo $this->_generateDateChooserFields('fabricated_event[start-date]', $post_meta['start-date']);?></td>
-				</tr>
-				<tr>
-					<th scope="row"><label for="end-date"><?php echo _e('End Date');?></label></th>
-					<td><?php echo $this->_generateDateChooserFields('fabricated_event[end-date]', $post_meta['end-date']);?></td>
-				</tr>
-			</table>
-			<h4>Event URL</h4>
-			<table class="form-table"> 
-				<tr>
-					<th scope="row"><label for="url"><?php echo _e('URL');?></label></th>
-					<td><input type="text" name="fabricated_event[url]" size="35" value="<?php echo (!empty($post_meta['url']) && $post_meta['url'] != 'http://' ? $post_meta['url'] : 'http://');?>" /> (e.g. http://www.website.com)</td>
-				</tr>
-			</table>
-			<h4>Contact Information</h4>
-			<table class="form-table">
-				<tr>
-					<th scope="row"><label for="contact-name"><?=_e('Name');?></label></th>
-					<td><input type="text" name="fabricated_event[contact-name]" size="35" value="<?php echo (!empty($post_meta['contact-name']) ? $post_meta['contact-name'] : '');?>" /> (e.g. John Doe)</td>
-				</tr>
-				<tr>
-					<th scope="row"><label for="contact-phone"><?=_e('Phone');?></label></th>
-					<td>(<input type="text" name="fabricated_event[contact-phone][0]" size="2" value="<?php echo (!empty($post_meta['contact-phone'][0]) ? $post_meta['contact-phone'][0] : '');?>" />) <input type="text" name="fabricated_event[contact-phone][1]" size="2" value="<?php echo (!empty($post_meta['contact-phone'][1]) ? $post_meta['contact-phone'][1] : '');?>" />-<input size="3"  type="text" name="fabricated_event[contact-phone][2]" value="<?php echo (!empty($post_meta['contact-phone'][2]) ? $post_meta['contact-phone'][2] : '');?>" /></td>
-				</tr>
-				<tr>
-					<th scope="row"><label for="contact-email"><?=_e('Email');?></label></th>
-					<td><input type="text" name="fabricated_event[contact-email]" size="35" value="<?php echo (!empty($post_meta['contact-email']) ? $post_meta['contact-email'] : '');?>" /> (e.g. sample@email.com)</td>
-				</tr>
-			</table><?
-		  	
-		}
-		
-		function saveFabricatedEventPostData($post_id) {
-			// Verify this came from the our screen and with proper authorization,
-			// because save_post can be triggered at other times.
-			if ( !wp_verify_nonce( $_POST['fabricatedevents_noncename'], FABRICATEDEVENTS_PLUGIN_URL )) {
-				return $post_id;
-			}
-			
-			// Verify if this is an auto save routine. If it is our form has not been submitted, so we dont want
-			// to do anything.
-			if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
-				return $post_id;
-			}
-			
-			// Check permissions
-			if ( 'page' == $_POST['post_type'] ) {
-				if ( !current_user_can( 'edit_page', $post_id ) ) {
-				  return $post_id;
-				}
-			} else {
-				if ( !current_user_can( 'edit_post', $post_id ) ) {
-				  return $post_id;
-				}
-			}
-			
-			// Convert the dates to timestamps;
-			$date = $_POST['fabricated_event']['start-date'];
-			
-			die(var_dump($_POST['fabricated_event']['start-date']));
-			
-			$_POST['fabricated_event']['start-date'] = mktime($date['hours'],$date['minutes'],0,$date['mon'],$date['day'],$date['year']);
-			
-			$date = $_POST['fabricated_event']['end-date'];
-			$_POST['fabricated_event']['end-date'] = mktime($date['hours'],$date['minutes'],0,$date['mon'],$date['day'],$date['year']);
-			
-			// Save the data.
-			update_post_meta($post_id, 'fabricated_event', $_POST['fabricated_event']);
-			
-			return $mydata;
-		}
-		
-		private function _generateDateChooserFields($fieldId, $values = null) {
-			$output = '';
-			
-			if ($values != null) {
-				$currentDate = getdate($values);
-			} else {
-				$currentDate = getdate();
-			}
-			
-			$output .= '<select name="'.$fieldId.'[mon]" id="'.$fieldId.'">';	  	
-			for ($i = 1; $i <= 12; $i++) {
-=======
 			
 			// HTML for the custom post metadata.
 			?><h4>Time &amp; Date</h4>
@@ -310,18 +220,14 @@ if (!class_exists('FabricatedEvents')) {
 				$i = sprintf("%02d", $i);
 				$currentDate['mon'] = sprintf("%02d", $currentDate['mon']);
 				
->>>>>>> Updated a few comments.
 				$output .= '<option value="'.$i.'" '.selected($i,$currentDate['mon'],false).'>'.date('M', mktime(0, 0, 0, $i+1, 0, 0, 0)).'</option>';
 			}
 			$output .= '</select> </label>
 			<select name="'.$fieldId.'[day]" id="'.$fieldId.'-day">';	  	
 			for ($i = 1; $i <= 31; $i++) {
-<<<<<<< HEAD
-=======
 				$i = sprintf("%02d", $i);
 				$currentDate['mday'] = sprintf("%02d", $currentDate['mday']);
 				
->>>>>>> Updated a few comments.
 				$output .= '<option value="'.$i.'" '.selected($i,$currentDate['mday'],false).'>'.$i.'</option>';
 			}
 			$output .= '</select>, 
@@ -332,24 +238,14 @@ if (!class_exists('FabricatedEvents')) {
 			$output .= '</select> @ 
 			<select name="'.$fieldId.'[hours]" id="'.$fieldId.'-hours">';	  	
 			for ($i = 1; $i <= 23; $i++) {
-<<<<<<< HEAD
-				$output .= '<option value="'.$i.'" '.selected($i,date('G'),false).'>'.$i.'</option>';
-=======
 				$i = sprintf("%02d", $i);
 				$currentDate['hours'] = sprintf("%02d", $currentDate['hours']);
 				
 				$output .= '<option value="'.$i.'" '.selected($i,$currentDate['hours'],false).'>'.$i.'</option>';
->>>>>>> Updated a few comments.
 			}
 			$output .= '</select> : 
 			<select name="'.$fieldId.'[minutes]" id="'.$fieldId.'-minutes">';	  	
 			for ($i = 1; $i <= 59; $i++) {
-<<<<<<< HEAD
-				$output .= '<option value="'.$i.'" '.selected($i,$currentDate['minutes'],false).'>'.$i.'</option>';
-			}
-			$output .= '</select>';
-			
-=======
 				$i = sprintf("%02d", $i);
 				$currentDate['minutes'] = sprintf("%02d", $currentDate['minutes']);
 				
@@ -357,7 +253,6 @@ if (!class_exists('FabricatedEvents')) {
 			}
 			$output .= '</select>';
 
->>>>>>> Updated a few comments.
 			return $output;
 		}
 	   
